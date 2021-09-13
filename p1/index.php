3812594252f39287f94b9f9e88bc8c693c06f714
owen-mcg <frm911@g.harvard.edu>
2021-09-13 17:14:32 +0000

    Resubmit

--- p1/index.php
+++ p1/index.php
@@ -1,3 +1,30 @@
-<h1>Practice App</h1>
 
-<img src="hes-logo.png">
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <title>Project 1</title>
+    <meta charset='utf-8'>
+</head>
+
+<body>
+
+    <h1>Project 1</h1>
+
+    <h2>Mechanics</h2>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+    <h2>Results</h2>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+</body>
+
+</html>
