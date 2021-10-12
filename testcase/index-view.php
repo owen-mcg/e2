a7ea0e2398784da4d17a48342b08b9fa135cb5cf
owen-mcg <frm911@g.harvard.edu>
2021-10-12 01:17:59 +0000

    debug

--- testcase/index-view.php
+++ testcase/index-view.php
@@ -18,31 +18,34 @@
 
 </html>
 
+
+
+//
 <!-- <body>
-
-    <h1>War (card game) Simulator</h1>
-
-    <h2>Mechanics</h2>
-
-    <ul>
-        <li>Each player starts with half the deck (26 cards), shuffled in a
-            random order.</li>
-        <li>For each round, a card is picked from the “top” of each player’s
-            cards.</li>
-        <li>Player whose card is highest wins that round and keeps both cards.
-        </li>
-        <li>If the two cards are of the same value, it’s a tie and those cards
-            are discarded.</li>
-        <li>The player who ends up with 0 cards is the loser.</li>
-    </ul>
-
-    <h2>Results</h2>
-
-    <ul>
-        <li>Rounds played: 128 </li>
-        <li>Winner: Player 2 </li>
-    </ul>
-
-    <h2>Rounds</h2>
-
-</body> -->
+// 
+    // <h1>War (card game) Simulator</h1>
+// 
+    // <h2>Mechanics</h2>
+// 
+    // <ul>
+        // <li>Each player starts with half the deck (26 cards), shuffled in a
+            // random order.</li>
+        // <li>For each round, a card is picked from the “top” of each player’s
+            // cards.</li>
+        // <li>Player whose card is highest wins that round and keeps both cards.
+        // </li>
+        // <li>If the two cards are of the same value, it’s a tie and those cards
+            // are discarded.</li>
+        // <li>The player who ends up with 0 cards is the loser.</li>
+    // </ul>
+// 
+    // <h2>Results</h2>
+// 
+    // <ul>
+        // <li>Rounds played: 128 </li>
+        // <li>Winner: Player 2 </li>
+    // </ul>
+// 
+    // <h2>Rounds</h2>
+// 
+// </body> -->
