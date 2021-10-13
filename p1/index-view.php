91ac819032013d8df38c892729c38dae3c8f6269
owen-mcg <frm911@g.harvard.edu>
2021-10-13 17:18:17 +0000

    clarity rearrange continued

--- p1/index-view.php
+++ p1/index-view.php
@@ -1,26 +1,18 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang='en'>
 
-<head>
-    <title>Project 1</title>
-    <meta charset='utf-8'>
-</head>
+    <head>
+        <title>Project 1</title>
+        <meta charset='utf-8'>
+    </head>
 
-<body>
+    <body>
+        <h1>Project 1</h1>
 
-    <h1>Project 1</h1>
-    <?php echo $filler;?>
+        <h2>Mechanics</h2>
 
-    <h2>Mechanics</h2>
-    <ul>
-        <li>Coming Soon!</li>
-    </ul>
-
-    <h2>Results</h2>
-    <ul>
-        <li>Arriving Shortly!</li>
-    </ul>
-
-</body>
+        <h2>Results</h2>
+        
+    </body>
 
 </html>
