c979475066b5f06b7e8a578b42bd3b053e74e4c4
owen-mcg <owen_mcgregor@outlook.com>
2021-11-23 15:35:33 +0000

    incremental update

--- zipfoods/app/Controllers/ProductsController.php
+++ zipfoods/app/Controllers/ProductsController.php
@@ -5,6 +5,16 @@ use App\Products;
 
 class ProductsController extends Controller
 {
+    private $productsObj;
+
+    # Create a construct method to set up a productsObj property that can be used across different methods
+    public function __construct($app)
+    {
+        parent::__construct($app);
+
+        $this->productsObj = new Products($this->app->path('database/products.json'));
+    }
+
     public function index()
     {
         $productsObj = new Products($this->app->path('database/products.json'));
@@ -12,6 +22,21 @@ class ProductsController extends Controller
 
         return $this->app->view('products/index', [
             'products' => $products
+        ]);
+    }
+
+    public function show()
+    {
+        $sku = $this->app->param('sku');
+
+        $product = $this->productsObj->getBySku($sku);
+
+        if (is_null($product)) {
+            return $this->app->view('errors/404');
+        }
+
+        return $this->app->view('products/show', [
+            'product' => $product
         ]);
     }
 }
--- zipfoods/routes.php
+++ zipfoods/routes.php
@@ -5,5 +5,6 @@
 return [
     # Ex: The path `/` will trigger the `index` method within the `AppController`
     '/' => ['AppController', 'index'],
-    '/products' => ['ProductsController', 'index']
+    '/products' => ['ProductsController', 'index'],
+    '/product' => ['ProductsController', 'show'],
 ];
