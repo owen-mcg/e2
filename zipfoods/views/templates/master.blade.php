e204e04fe0f79d0b36b41b5ff3734f85e01e76d6
owen-mcg <owen_mcgregor@outlook.com>
2021-11-23 05:01:56 +0000

    tryna get it set up

--- zipfoods/views/templates/master.blade.php
+++ zipfoods/views/templates/master.blade.php
@@ -7,7 +7,8 @@
     <meta charset='utf-8'>
 
     <link rel='shortcut icon' href='/favicon.ico'>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href='/css/zipfoods.css' rel='stylesheet'>
     <link href='/css/app.css' rel='stylesheet'>
 
     @yield('head')
@@ -16,7 +17,7 @@
 <body>
 
 <header>
-    <img id='logo' src='/images/hes-logo.png' alt='Harvard Extension School Logo'>
+    <a href='/'><img id='logo' src='/images/zipfoods-logo.png' alt='{{ $app->config('app.name') }} logo'></a>
     <h1>{{ $app->config('app.name') }}</h1>
 </header>
 
