515a487eee9e848363e4edac5383ba73f0f86054
owen-mcg <owen_mcgregor@outlook.com>
2021-12-01 04:05:49 +0000

    More things I missed or forgot to commit

--- zipfoods/views/templates/master.blade.php
+++ zipfoods/views/templates/master.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang='en'>
+
 <head>
 
     <title>@yield('title', $app->config('app.name'))</title>
@@ -7,25 +8,28 @@
     <meta charset='utf-8'>
 
     <link rel='shortcut icon' href='/favicon.ico'>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href='/css/zipfoods.css' rel='stylesheet'>
-    <link href='/css/app.css' rel='stylesheet'>
 
     @yield('head')
 
 </head>
+
 <body>
 
-<header>
-    <a href='/'><img id='logo' src='/images/zipfoods-logo.png' alt='{{ $app->config('app.name') }} logo'></a>
-    <h1>{{ $app->config('app.name') }}</h1>
-</header>
+    <header>
+        <h1><a href='/'><img id='logo' src='/images/zipfoods-logo.png' alt='{{ $app->config('app.name') }} Logo'></a>
+        </h1>
+    </header>
 
-<main>
-    @yield('content')
-</main>
+    <main>
+        @yield('content')
+    </main>
 
-@yield('body')
+    @yield('body')
 
 </body>
+
 </html>
